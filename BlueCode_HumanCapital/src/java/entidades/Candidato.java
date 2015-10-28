d78bb7e8a548959b16e3dbac79e5dd5f369d046b
Cesar <cesars90_@hotmail.com>
2015-10-28 11:05:15 -0600

    Agregada clase de Candidato

---
+++ BlueCode_HumanCapital/src/java/entidades/Candidato.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidades;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Cesar
+ */
+public class Candidato implements Serializable {
+    int id;
+    String nombreDePila;
+    String apellidos;
+    String expectativasEconomicas;
+    String direccion;
+    String telefono;
+    String titulo;
+    String email;
+
+    public Candidato(int id, String nombreDePila, String apellidos, String expectativasEconomicas, String direccion, String telefono, String titulo) {
+        this.id = id;
+        this.nombreDePila = nombreDePila;
+        this.apellidos = apellidos;
+        this.expectativasEconomicas = expectativasEconomicas;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.titulo = titulo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombreDePila() {
+        return nombreDePila;
+    }
+
+    public void setNombreDePila(String nombreDePila) {
+        this.nombreDePila = nombreDePila;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getExpectativasEconomicas() {
+        return expectativasEconomicas;
+    }
+
+    public void setExpectativasEconomicas(String expectativasEconomicas) {
+        this.expectativasEconomicas = expectativasEconomicas;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+}
---
+++ BlueCode_HumanCapital/web/crear_candidato.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : crear_candidato
+    Created on : Oct 28, 2015, 10:59:38 AM
+    Author     : Cesar
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Crear candidato</title>
+    </head>
+    <body>
+        <h1>Nuevo Candidato</h1>
+    </body>
+</html>
