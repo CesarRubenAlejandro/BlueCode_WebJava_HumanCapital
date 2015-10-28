d3b1c0df6fd143515081e500fe326c4c78d737cd
Eduardo Zardain <ejzardainc@gmail.com>
2015-10-28 14:17:12 -0600

    Agrega el script de sql con los datos de prueba para la base de datos

---
+++ BlueCode_HumanCapital/seeds.sql
@@ -0,0 +1,150 @@
+
+USE capital_humano;
+################################################################################
+
+-- Unicamente Candidatos
+INSERT INTO Candidatos VALUES (
+  NULL,
+  'Giacomo',
+  'Guilizzoni',
+  'Ingeniero en Pizzas',
+  'Universitá della Pizza di Milano',
+  'giacomo@gmail.com',
+  '10218392',
+  'Coliseo Romano 435 Col. Roma',
+  '4000/mes',
+  0
+);
+
+INSERT INTO Candidatos VALUES (
+  NULL,
+  'Marco',
+  'Botton Tuttofare',
+  'Ingeniero en Sistemas',
+  'Politecnico di Milano',
+  'marco@marco.com',
+  '5551890321',
+  'Piazza Leonardo da Vinci, 32',
+  '6000/mes',
+  1
+);
+
+INSERT INTO Candidatos VALUES (
+  NULL,
+  'Mariah',
+  'Maclachlan',
+  'Ingeniero Civil',
+  'Arizona State University',
+  'mmaclachlan@gmail.com',
+  '5782214592',
+  'Av Fernando Garcia Roel 54',
+  '5000/mes',
+  2
+);
+################################################################################
+
+-- Candidatos que ya son empleados
+INSERT INTO Candidatos VALUES (
+  NULL,
+  'Memo',
+  'Ochoa',
+  'Ingeniero en Redes y Porterias',
+  'Universidad Televisora de Nayarit SA',
+  'memo_temo@televisa.com',
+  '428379932',
+  'Estadio Azteca 123',
+  '50000/mes',
+  1
+);
+
+INSERT INTO Candidatos VALUES (
+  NULL,
+  'Pedrito',
+  'Tutto',
+  'Ingeniero en Tecnologías de la Información',
+  'Instituto Politécnico Nacional',
+  'pedriot@marco.com',
+  '4359676512',
+  'Av Insurgentes Nte 2345',
+  '40000/mes',
+  1
+);
+
+INSERT INTO Candidatos VALUES (
+  NULL,
+  'Sara',
+  'Sarah',
+  'Licenciado en Peluqueria',
+  'Universidad Televisora de Nayarit SA',
+  'sarasarah@gmail.com',
+  '654902942',
+  'Junco de la Vega 432',
+  '10000/mes',
+  1
+);
+################################################################################
+
+-- Certificados y Trabajos Anteriores
+INSERT INTO Certificados VALUES (1, 'Certificado en Salsa Roja 2013-2014');
+
+INSERT INTO TrabajosAnteriores VALUES (
+  1, 
+  'Ingeniero en Software',
+  'Desarrollo de sistema de pizzeria'
+);
+
+INSERT INTO TrabajosAnteriores VALUES (
+  4, 
+  'Ingeniero en Software',
+  'Desarrollo de sistema de redes'
+);
+################################################################################
+
+-- Empleados
+INSERT INTO Empleados VALUES (
+  4,
+  44000,
+  'Portero',
+  10
+);
+
+INSERT INTO Empleados VALUES (
+  5,
+  30000,
+  'Programador',
+  7
+);
+
+INSERT INTO Empleados VALUES (
+  6,
+  15000,
+  'Peluquera',
+  14
+);
+################################################################################
+
+-- Entrevistadores
+INSERT INTO Entrevistadores VALUES (6);
+################################################################################
+
+-- Administradores
+INSERT INTO Administradores VALUES (6, 'sara', '1234');
+################################################################################
+
+-- Entrevistas
+INSERT INTO Entrevistas VALUES (
+  4,
+  6,
+  '2015-10-01',
+  'Skype',
+  'Muy buena entrevista'
+);
+
+INSERT INTO Entrevistas VALUES (
+  1,
+  6,
+  '2015-10-10',
+  'Skype',
+  'El candidato hablo en italiano y no le entendi'
+);
+################################################################################
