a3e0f24d3998c4567a53925e1c0599742af1525d
pianoricky@gmail.com <pianoricky@gmail.com>
2015-12-03 13:59:38 -0600

    login casi terminado

---
+++ BlueCode_HumanCapital/src/java/values/Keys.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package values;
+
+/**
+ *
+ * @author Ricky
+ */
+public class Keys {
+    /* Para que la clase no se pueda instanciar */
+    private Keys() {}
+    
+    public static final String USERNAME = "username";
+    public static final String PASSWORD = "password";
+    public static final String LOGIN_SUCCESS = "login_success";
+}
